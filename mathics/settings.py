7a010fe7d5212c8dd6f3498d0be94286a70e6064
Angus Griffith <16sn6uv@gmail.com>
2016-02-08 16:56:01 +0100

    cleanup settings.py

--- mathics/settings.py
+++ mathics/settings.py
@@ -18,39 +18,46 @@ u"""
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+#------------------------------------------------------------------------------
+# Mathics runtime configuration
+#------------------------------------------------------------------------------
+
+# Recusion depth limit
+MAX_RECURSION_DEPTH = 512
+
+# Number of bits of precision for inexact calculations
+MACHINE_PRECISION = 64
+
+# Leave this True unless you have specific reason for not permitting users to
+# access local files (like running mathics on a public webserver).
+ENABLE_FILES_MODULE = True
+
+# Time in seconds to allow for each execution. Set to `None` to disable.
+TIMEOUT = None
+
+# Set this True if you prefer 12 hour time to be the default
+TIME_12HOUR = False
+
+# Local time zone for this installation.
+# On Windows set this to the same as your system time zone.
+TIME_ZONE = 'Europe/Vienna'
+
+#------------------------------------------------------------------------------
+# System settings - don't edit unless you know what you are doing
+#------------------------------------------------------------------------------
+
 import pkg_resources
 import sys
 import os
-from os import path
 
-
-TIMEOUT = None
-MAX_RECURSION_DEPTH = 512
-
-# number of bits of precision for inexact calculations
-MACHINE_PRECISION = 64
-
+# Root directory of mathics installation
 ROOT_DIR = pkg_resources.resource_filename('mathics', '') + '/'
 if sys.platform.startswith('win'):
     DATA_DIR = os.environ['APPDATA'].replace(os.sep, '/') + '/Python/Mathics/'
 else:
-    DATA_DIR = path.expanduser('~/.local/var/mathics/')
+    DATA_DIR = os.path.expanduser('~/.local/var/mathics/')
 
 DOC_DIR = ROOT_DIR + 'doc/documentation/'
 DOC_TEX_DATA = ROOT_DIR + 'doc/tex/data'
 DOC_XML_DATA = ROOT_DIR + 'doc/xml/data'
 DOC_LATEX_FILE = ROOT_DIR + 'doc/tex/documentation.tex'
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'Europe/Vienna'
-
-# Set this True if you prefer 12 hour time to be the default
-TIME_12HOUR = False
-
-# Leave this True unless you have specific reason for not permitting
-# users to access local files
-ENABLE_FILES_MODULE = True
